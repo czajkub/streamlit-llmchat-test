d796918f61597612125571e49094d0bff16bb123
czajkub <czajkub@gmail.com>
2025-09-17 13:01:38 +0200

    added agent to chat

--- gui.py
+++ gui.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
+from app.agent import AgentManager
+
+agent_manager = AgentManager()
+
+
 st.title("ChatDziPiTi")
 
 st.markdown(
     """
-    This is definitely chatgpt. pleasee click me
+    This is definitely chatgpt. pleasee chat with me
     """,
 )
-if st.button("Chat"):
-    st.markdown("Ask me something!")
+
+
+async def runner():
+    await agent_manager.initialize()
+
+    while True:
+        user_input = st.text_input("Ask something!")
+        response = await agent_manager.handle_message(user_input, verbose=True)
+        st.write(response)
+
