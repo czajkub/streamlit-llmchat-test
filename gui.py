f9cb9c2f1da8c9c07c9d5204109878c58ab609a2
czajkub <czajkub@gmail.com>
2025-09-22 09:11:44 +0200

    nawonr

--- gui.py
+++ gui.py
@@ -1,48 +1,76 @@
-import os
-import streamlit as st
-os.environ["API_KEY"] = st.secrets["API_KEY"]
-
 import asyncio
 import time
+import os
 
-import nest_asyncio
+import streamlit as st
+from streamlit.errors import StreamlitSecretNotFoundError
+from openai import AuthenticationError
+
+from app.config import settings
+os.environ["API_KEY"] = settings.api_key
+
+# Try to authenticate with streamlit secrets
+# if .env doesn't provide an api key
+if settings.api_key == "":
+    try:
+        os.environ["API_KEY"] = st.secrets["API_KEY"]
+    except StreamlitSecretNotFoundError:
+        st.warning("Provide a valid API key.")
+        raise
+
+if "MCP_URL" in st.secrets:
+    os.environ["MCP_URL"] = st.secrets["MCP_URL"]
 
 from app.agent import AgentManager
 
-nest_asyncio.apply()
-
 
 agent_manager = AgentManager()
-if os.environ.get("API_KEY") is None:
-    st.markdown("Please provide an API key to continue...")
-else:
-    try:
-        asyncio.run(agent_manager.initialize())
-    except ConnectionError:
-        st.markdown("Connection refused...")
 
-st.image("cat.jpg", width=100)
-st.title("ChatDżiPiTi")
+try:
+    asyncio.run(agent_manager.initialize())
+except ExceptionGroup:
+    st.markdown("Failed to initialize the agent...")
 
-st.markdown(
-    """
-    ### Welcome to chatMCP. In this chat, you can:
-      - query your apple-health data
-      - talk to your new imaginary best friend
-    """,
-)
+
+if "chats" not in st.session_state:
+    st.session_state.chats = 1
+if "active_chat" not in st.session_state:
+    st.session_state.active_chat = 1
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+st.title("Your AI Assistant")
+
 
 st.divider()
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
 
-for message in st.session_state["messages"]:
+# ---------- sidebar ----------
+
+with st.sidebar:
+    if st.button("New chat"):
+        st.session_state.chats += 1
+        st.session_state.active_chat = st.session_state.chats
+    st.divider()
+    st.session_state.chat_buttons = []
+    for i in range(1, st.session_state["chats"] + 1):
+        if st.button(f"Chat {i}", key=f"chat{i}"):
+            st.session_state.active_chat = i
+
+# -----------------------------
+
+
+if f"messages{st.session_state["active_chat"]}" not in st.session_state:
+    st.session_state[f"messages{st.session_state["chats"]}"] = []
+
+for message in st.session_state[f"messages{st.session_state["active_chat"]}"]:
     with st.chat_message(message["role"]):
         st.markdown(message["text"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "human", "text": prompt})
+if prompt := st.chat_input("Ask me something"):
+    st.session_state[f"messages{st.session_state["active_chat"]}"].append({"role": "human", "text": prompt})
+
     with st.chat_message("human"):
         st.markdown(prompt)
 
@@ -53,13 +81,22 @@ if prompt := st.chat_input("What is up?"):
         try:
             with st.spinner("running...", show_time=True):
                 response = asyncio.run(agent_manager.handle_message(prompt))
-        except ConnectionError:
-            st.warning("Connection refused.")
+        except AuthenticationError:
+            st.warning("Please provide an api key in .env")
+            response = ""
+        except (ExceptionGroup, RuntimeError):
+            st.warning("Failed to connect with MCP server.")
+            response = ""
 
-
+        # Split response into chunks to imitate AI behaviour
         for chunk in response.split():
             full_response += chunk + " "
             time.sleep(0.04)
             placeholder.markdown(full_response + "▌")
         placeholder.markdown(full_response)
-        st.session_state.messages.append({"role": "assistant", "text": full_response})
+        st.session_state[f"messages{st.session_state["active_chat"]}"].append({"role": "assistant", "text": full_response})
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
